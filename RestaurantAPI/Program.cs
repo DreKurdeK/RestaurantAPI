86dce4c16bb42f717823f7630df3d3c16910b55e
Daniel <Daniel@DANIEL>
2024-10-04 13:14:35 +0200

    Add project files.

---
+++ RestaurantAPI/Program.cs
@@ -0,0 +1,23 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
