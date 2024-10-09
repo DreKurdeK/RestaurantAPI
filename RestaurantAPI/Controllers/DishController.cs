b778f07c161ec60441201f565697ad861a5bf3c9
Daniel <radziak.daniel@o2.pl>
2024-10-09 19:13:24 +0200

    next changes

--- RestaurantAPI/Controllers/DishController.cs
+++ RestaurantAPI/Controllers/DishController.cs
@@ -4,15 +4,44 @@ using RestaurantAPI.Services;
 
 namespace RestaurantAPI.Controllers
 {
-    [Route("api/{restaurantId}/dish")]
+    [Route("api/restaurant/{restaurantId}/dish")]
     [ApiController]
     public class DishController(IDishService dishSerivce) : ControllerBase
     {
         [HttpPost]
-        public ActionResult Post([FromRoute]int restaurantId, CreateDishDto dto)
+        public ActionResult Post([FromRoute]int restaurantId, [FromBody]CreateDishDto dto)
         {
             var newDishId = dishSerivce.Create(restaurantId, dto);
-            return Created("api/{restaurantId}/dish/{newDishId}", null);
+            return Created($"api/restaurant/{restaurantId}/dish/{newDishId}", null);
         }
+
+        [HttpGet("{dishId}")]
+        public ActionResult<DishDto> Get([FromRoute] int restaurantId, [FromRoute] int dishId)
+        {
+            DishDto dish = dishSerivce.GetById(restaurantId, dishId);
+            return Ok(dish);
+        }
+
+        [HttpGet]
+        public ActionResult<List<DishDto>> Get([FromRoute] int restaurantId)
+        {
+            var result = dishSerivce.GetAll(restaurantId);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public ActionResult Delete([FromRoute] int restaurantId)
+        {
+            dishSerivce.RemoveAll(restaurantId);
+            return NoContent();
+        }
+
+        [HttpDelete("{dishId}")]
+        public ActionResult DeleteFromId([FromRoute] int restaurantId, [FromRoute] int dishId)
+        {
+            dishSerivce.RemoveById(restaurantId, dishId);
+            return NoContent();
+        }
+
     }
 }
--- RestaurantAPI/Services/DishService.cs
+++ RestaurantAPI/Services/DishService.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 using RestaurantAPI.Entities;
 using RestaurantAPI.Entities.Exceptions;
 using RestaurantAPI.Models;
@@ -8,20 +10,77 @@ namespace RestaurantAPI.Services
     public interface IDishService
     {
         int Create(int restaurantId, CreateDishDto dto);
+        List<DishDto> GetAll(int restaurantId);
+        DishDto GetById(int restaurantId, int dishId);
+        void RemoveAll(int restaurantId);
+        void RemoveById(int restaurantId, int dishId);
     }
 
     public class DishService(RestaurantDbContext context, IMapper mapper) : IDishService
     {
         public int Create(int restaurantId, CreateDishDto dto)
         {
-            var restaurant = context.Restaurants.FirstOrDefault(r => r.Id == restaurantId);
-            if (restaurant == null)
-                throw new NotFoundException("Restaurant not found");
+            _ = GetRestaurantById(restaurantId);
 
             var dishEntity = mapper.Map<Dish>(dto);
+
+            dishEntity.RestaurantId = restaurantId;
+
             context.Dishes.Add(dishEntity);
             context.SaveChanges();
             return dishEntity.Id;
+        }
+
+        public DishDto GetById(int restaurantId, int dishId)
+        {
+            var restaurant = GetRestaurantById(restaurantId);
+
+            var dish = context.Dishes.FirstOrDefault(d => d.Id == dishId);
+            if (dish is null || dish.RestaurantId != restaurantId)
+                throw new NotFoundException("Dish not found");
+
+            var dishDto = mapper.Map<DishDto>(dish);
+            return dishDto;
+        }
+
+        public List<DishDto> GetAll(int restaurantId)
+        {
+            var restaurant = GetRestaurantById(restaurantId);
+            var dishDtos = mapper.Map<List<DishDto>>(restaurant.Dishes);
+
+            return dishDtos;
+        }
+
+        public void RemoveAll(int restaurantId)
+        {
+            var restaurant = GetRestaurantById(restaurantId);
+
+            context.RemoveRange(restaurant.Dishes);
+            context.SaveChanges();
+        }
+
+        public void RemoveById(int restaurantId, int dishId)
+        {
+            var restaurant = GetRestaurantById(restaurantId);
+
+            var dish = context.Dishes.FirstOrDefault(d => d.Id == dishId && d.RestaurantId == restaurantId);
+            if (dish is null)
+                throw new NotFoundException("Dish not found");
+
+            context.Dishes.Remove(dish);
+            context.SaveChanges();
+        }
+
+        private Restaurant GetRestaurantById(int restaurantId)
+        {
+            var restaurant = context
+                .Restaurants
+                .Include(r => r.Dishes)
+                .FirstOrDefault(r => r.Id == restaurantId);
+            if (restaurant is null)
+                throw new NotFoundException("Restaurant not found");
+
+            return restaurant;
         }
     }
 }
